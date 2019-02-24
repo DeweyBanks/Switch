a40e24a8adfbd23ce697f147d3474ba3f14ff808
deweybanks <deweybanks@gmail.com>
2019-02-24 13:57:06 -0500

    adds validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :devices
   has_one :reservation, dependent: :destroy
-
+  validates_presence_of :reservation
   accepts_nested_attributes_for :reservation
   accepts_nested_attributes_for :devices, reject_if: :all_blank, allow_destroy: true
 end
